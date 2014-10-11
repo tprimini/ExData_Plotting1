ad619ae1aaa85b7a63fb3a9da072d2b9c6163920
Tiago Octaviano Primini <tiagopri@cpqd.com.br>
2014-10-11 18:04:20 -0300

    week1
    Change-Id: I8c329b413d07f264fff87dd5f5a47d44f8416047

--- plot4.R
+++ plot4.R
@@ -27,7 +27,7 @@ with(data, {
   plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~DateTime,col='Red')
   lines(Sub_metering_3~DateTime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", cex=0.3, col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~DateTime, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
 })
 
